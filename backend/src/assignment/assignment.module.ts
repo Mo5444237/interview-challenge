722bafca9716df9b2065864acfa947f5c7d83f79
Mohammed ibrahim <mo5444237@gmail.com>
2025-07-12 20:41:17 +0200

    mo5444237/add logic for assignment module

--- backend/src/assignment/assignment.module.ts
+++ backend/src/assignment/assignment.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AssignmentController } from './assignment.controller';
 import { AssignmentService } from './assignment.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Assignment } from './entities/assignment.entity';
+import { Patient } from 'src/patient/entities/patient.entity';
+import { Medication } from 'src/medication/entities/medication.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Assignment, Patient, Medication])],
   controllers: [AssignmentController],
   providers: [AssignmentService],
 })
